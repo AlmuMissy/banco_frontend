2a7dd3af3b5bf5551c79e37f6633c7e5a4e5d7ff
Almudena <almufullstack@gmail.com>
2022-06-20 18:52:34 +0200

    copia de seguridad

--- main.js
+++ main.js
@@ -19,64 +19,27 @@ Ejercicio 4.1: El resto de funcionalidades anteriores del programa deben abstrae
 */
 
 // las propiedades del objeto opciones son: url, metodo, body, cabeceras
-const server='localhost:8085';
-const opciones = {
-    url:'http://localhost:8085/ok',
-    metodo:'GET'
+
+
+
+
+
+// obtenerGestores(gestores => {
+//     mostrarGestores(gestores);
+// });
+
+async function main() {
+
+    const gestores = await obtenerGestores();
+    mostrarGestores(gestores);
 }
 
-ajax(opciones, (data) => {
-    console.log(data);
-})
+main();
 
-
-const opcionesLogin= {
-
-    url:`http://${server}/login/gestor/`,
-    metodo: 'POST',
-    body :'usuario=gestor1&password=gestor1',
-    cabeceras:{
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-}
-
-
-//realiza la petición de login del gestor
-ajax(opcionesLogin, (data) => {
-
-    // esta función se ejecuta cuando los datos han sido
-    // devueltos por el servicio
-
-    // convirtiendo el string en formato JSON a objeto de JavaScript
-    const respuesta = JSON.parse(data);
-
-    // guardamos el token
-    const token = respuesta.data.token;
-
-    // realizamos la solicitud para obtener todos los gestores
-
-    const opcionesObtenerGestores = {
-        url: 'http://localhost:8085/gestores/',
-        metodo: 'GET',
-        cabeceras: {
-            Authorization: `Basic ${token}`
-        }
-    }
-
-    ajax(opcionesObtenerGestores, (data) => {
-        console.log(data);
-
-        // data tiene toda la información de los gestores en formato string
-        console.log(data);
-
-        // convertir a objeto de JavaScript
-        const respuesta = JSON.parse(data);
-
-        const gestores = respuesta.data;
-        mostrarGestores(gestores);
-
-
-
-    });
-});
+//funcion ejecutable
+//(async ()=> {
+    
+//        const gestores = await obtenerGestores();
+//        mostrarGestores(gestores);
+//});
 
